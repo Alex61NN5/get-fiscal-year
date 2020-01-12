d9647f8f3bfde28fb4a6b22d407ad38bd7a06c47
Alex Ginns <alexginns@gmail.com>
2020-01-12 18:04:27 +1100

    refactor: basic method setup

--- index.js
+++ index.js
@@ -1,14 +1,59 @@
+import fiscalData from './fiscalData.js';
+
 let userLocation;
 
-getUserLocation = () => {
-  fetch('http://ip-api.com/json/')
-    .then(res => res.json())
-    .then(response => {
-      userLocation = response;
-    })
-    .catch((err) => {
-      console.error('Request Failed - This may be caused by an AdBlocker. Please disable or enter a valid country.');
-    })
+const getUserLocation = async () => {
+  try {
+    let response = await fetch('http://ip-api.com/json/');
+    let data = await response.json();
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-getUserLocation();
+const getFiscalYear = async (country = null, period = 'current') => {
+  if (country) {
+    eval(period + 'FiscalYear')(getCountryInfo(country));
+  } else {
+    if (!userLocation) {
+      getUserLocation()
+        .then((result) => {
+          userLocation = result;
+          eval(period + 'FiscalYear')(getCountryInfo(userLocation.countryCode));
+        })
+        .catch((err) => {
+          console.error(err);
+        })
+    } else {
+      eval(period + 'FiscalYear')(getCountryInfo(userLocation.countryCode));
+    }
+  }
+}
+
+const getCountryInfo = (country) => {
+  if (country.split('').length > 2) {
+    let c = country.toLowerCase();
+    return fiscalData.data.filter(x => x.country.toLowerCase().includes(c))[0];
+  } else {
+    return fiscalData.data.filter(x => x.code === country)[0];
+  }
+}
+
+const lastFiscalYear = (country) => {
+  console.log(country);
+  // get last financial year
+}
+
+const currentFiscalYear = (country) => {
+  console.log(country);
+  // get current financial year
+}
+
+const nextFiscalYear = (country) => {
+  console.log(country);
+  // get next financial year
+}
+
+// getUserLocation();
+getFiscalYear('New Zealand', 'last');
